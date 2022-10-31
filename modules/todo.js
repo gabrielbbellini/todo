988a12234430d5ec2f8482929b62e14309caa4b3
Gabriel Bellini <gabrielbritobellini@gmail.com>
2022-10-31 12:36:55 -0300

    change function name to be more intuitive

--- modules/todo.js
+++ modules/todo.js
@@ -1,26 +1,25 @@
-const { query } = require("express");
 const database = require("../configs/database/database");
 
 class TodoModule {
     createTask = (task, response) => {
         const query = "INSERT INTO task (title, description) VALUES (?, ?)";
         const values = [task.title, task.description];
-        const callback = (error, result) => {
+        const executeOnFinishQuery = (error, _) => {
             if(error) {
                 response.status(500).send(error);
                 return;
             }
 
-            response.status(200).json({ message: "Task successfully addded."});
+            response.status(200).json({ message: "Task successfully added."});
         };
 
-        database.query(query, values, callback);
+        database.query(query, values, executeOnFinishQuery);
     };
 
     
     listTasks(response) {
         const query = "SELECT id, title, description, modified_at FROM task";
-        const callback = (error, result) => {
+        const executeOnFinishQuery = (error, result) => {
             if(error) {
                 response.status(500).send(error);
                 return;
@@ -29,13 +28,13 @@ class TodoModule {
             response.status(200).json(result);
         };
 
-        database.query(query, null, callback);
+        database.query(query, null, executeOnFinishQuery);
     };
 
     getTask(taskId, response) {
         const query = "SELECT id, title, description, modified_at FROM task WHERE id = ?";
         const values = [taskId];
-        const callback = (error, result) => {
+        const executeOnFinishQuery = (error, result) => {
             if(error) {
                 response.status(500).send(error);
                 return;
@@ -44,13 +43,13 @@ class TodoModule {
             response.status(200).json(result[0]);
         };
 
-        database.query(query, values, callback);
+        database.query(query, values, executeOnFinishQuery);
     };
 
     updateTask(task, response) {
         const query = "UPDATE task SET title = ?, description = ? WHERE id = ?";
         const values = [task.title, task.description, task.id];
-        const callback = (error, result) => {
+        const executeOnFinishQuery = (error, result) => {
             if(error) {
                 response.status(500).send(error);
                 return;
@@ -59,13 +58,13 @@ class TodoModule {
             response.status(200).json(result);
         };
 
-        database.query(query, values, callback);
+        database.query(query, values, executeOnFinishQuery);
     };
 
     deleteTask(taskId, response) {
         const query = "DELETE FROM task WHERE id = ?";
         const values = [taskId];
-        const callback = (error, result) => {
+        const executeOnFinishQuery = (error, result) => {
             if(error) {
                 response.status(500).send(error);
                 return;
@@ -74,7 +73,7 @@ class TodoModule {
             response.status(200).json(result);
         };
 
-        database.query(query, values, callback);
+        database.query(query, values, executeOnFinishQuery);
     };
 }
 
